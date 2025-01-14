419ff017ee8878ad514ea37c7ba9339989d8a2df
Arina <Ukam.naletko@mail.ru>
2025-01-14 23:42:23 +0300

    create

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// function nextSection(sectionNumber) {
+//    
+//     const sections = document.querySelectorAll('.section');
+//     sections.forEach(section => {
+//         section.style.display = 'none';
+//     });
+//     const selectedSection = document.getElementById(`section${sectionNumber}`);
+//     selectedSection.style.display = 'block';
+// }
